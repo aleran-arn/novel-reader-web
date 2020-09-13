7558d3f2a2a835fe78abf421597a0f9b1b8b92a7
Arnold <atammemiagi@gmail.com>
2020-09-13 16:32:00 +0300

    add

--- controller/NovelController.js
+++ controller/NovelController.js
@@ -1,20 +1,20 @@
 //Import Novel Model
 var Novel = require('@novelreader/core/model/Novel');
 //For list
-exports.list = function (req, res) {
-    Novel.get(req.params.limit)
-        .then((err, novels) => {
-            if (err)
-                res.json({
-                    status: "error",
-                    message: err
-                });
-            res.json({
-                status: "success",
-                message: "Got Novel Successfully!",
-                data: novels
-            });
+exports.list = async function (req, res) {
+    try {
+        const novels = await Novel.get(req.params.limit);
+        res.json({
+            status: "success",
+            message: "Got Novel Successfully!",
+            data: novels
         });
+    } catch (err) {
+        res.json({
+            status: "error",
+            message: err
+        });
+    }
 };
 // View Novel
 exports.view = function (req, res) {
